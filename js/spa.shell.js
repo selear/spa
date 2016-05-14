91ad31e5786595122e2d3cdd754c038038eab9b2
selear <selear@gmail.com>
2016-05-14 17:11:49 +0800

    编写shell的样式表
    3.4.2@P71

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -9,18 +9,18 @@ spa.shell = (function () {
   //----- Begin Module scope variables -----
   var configMap = {
         main_html: new String() +
-            '<div class="spa-shell-head">' +
-            '<div class="spa-shell-head-logo"></div>' +
-            '<div class="spa-shell-head-acct"></div>' +
-            '<spa class="spa-shell-head-search"></spa>' +
-            '</div>' +
-            '<div class="spa-shell-main">' +
-            '<div class="spa-shell-main-nav"></div>' +
-            '<div class="spa-shell-main-content"></div>' +
-            '</div>' +
-            '<div class="spa-shell-foot"></div>' +
-            '<div class="spa-shell-chat"></div>' +
-            '<div class="spa-shell-modal"></div>'
+          + '<div class="spa-shell-head">'
+            + '<div class="spa-shell-head-logo"></div>'
+            + '<div class="spa-shell-head-acct"></div>'
+            + '<spa class="spa-shell-head-search"></spa>'
+          + '</div>'
+          + '<div class="spa-shell-main">'
+            + '<div class="spa-shell-main-nav"></div>'
+            + '<div class="spa-shell-main-content"></div>'
+          + '</div>'
+          + '<div class="spa-shell-foot"></div>'
+          + '<div class="spa-shell-chat"></div>'
+          + '<div class="spa-shell-modal"></div>'
     },
         stateMap = { $container : null },
         jqueryMap = {},
