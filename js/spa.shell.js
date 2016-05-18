560896d5c25c38537d3bfdadc5e7b2aa23a3c5eb
selear <selear@gmail.com>
2016-05-18 15:17:37 +0800

    应用uriAnchor给应用添加状态
    3.6.3@P82
    每当chat的状态发生改变, 修改uri, 使得应用状态能够反映在uri上, 方便回溯乃至使用浏览器按钮

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -100,7 +100,11 @@ spa.shell = (function () {
 
     //+++++ event handlers +++++
     onClickChat = function (event) {
-      toggleChat(stateMap.is_chat_retracted);
+      if(toggleChat(stateMap.is_chat_retracted)) {
+        $.uriAnchor.setAnchor({
+          chat: (stateMap.is_chat_retracted ? 'open' : 'closed')
+        });
+      }
       return false;
     };
     //----- event handlers -----
